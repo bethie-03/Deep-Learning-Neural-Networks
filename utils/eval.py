7f2cbff50b45300961ffdd57d21d3944789b7ea3
bethie03 <phuongtrang4583@gmail.com>
2024-08-03 15:43:14 +0700

    Change model to eval mode, calculate the loss on test set

--- utils/eval.py
+++ utils/eval.py
@@ -1,21 +1,26 @@
 import torch
 
-def inference(test_loader, classifier, device):
+def evaluate(test_loader, classifier, criterion, device):
+    classifier.eval()  
     with torch.no_grad():
-            correct = 0
-            total = 0
+        correct = 0
+        total = 0
+        total_loss = 0.0  
 
-            for images, labels in test_loader:
-                images = images.to(device)
-                labels = labels.to(device)
-                
-                outputs = classifier(images)
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            outputs = classifier(images)
+            loss = criterion(outputs, labels)  
 
-                _, predicted = torch.max(outputs.data, 1)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            total_loss += loss.item()  
 
-                total += labels.size(0)
+        accuracy = (correct / total) * 100
+        average_loss = total_loss / total  
 
-                correct +=(predicted == labels).sum().item()
-                del images, labels, outputs
-
-            print(f'Accuracy of the model on test data: {(correct/total)*100}%')
+        print(f'Accuracy of the model on test data: {accuracy:.2f}%')
+        print(f'Average loss of the model on test data: {average_loss:.4f}')
