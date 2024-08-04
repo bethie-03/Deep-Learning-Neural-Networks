eb01bee39773db11a0c5e279dcfa3719610aa758
bethie03 <phuongtrang4583@gmail.com>
2024-08-05 01:22:27 +0700

    Calculate other metrics beside loss and accuracy.

--- utils/eval.py
+++ utils/eval.py
@@ -1,26 +1,31 @@
 import torch
 
-def evaluate(test_loader, classifier, criterion, device):
-    classifier.eval()  
+def evaluate(test_loader, classifier, metrics, criterion, device):
+    classifier.eval()
+
     with torch.no_grad():
-        correct = 0
-        total = 0
-        total_loss = 0.0  
+        total_loss = 0.0
+    
+        for metric in metrics.values():
+                metric.reset()
 
         for images, labels in test_loader:
             images = images.to(device)
             labels = labels.to(device)
-            
+
             outputs = classifier(images)
-            loss = criterion(outputs, labels)  
+            loss = criterion(outputs, labels)
 
             _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            total_loss += loss.item()  
+            total_loss += loss.item()
+            
+            for metric in metrics.values():
+                metric.update(predicted, labels)
 
-        accuracy = (correct / total) * 100
-        average_loss = total_loss / len(test_loader)  
-
-        print(f'Accuracy of the model on test data: {accuracy:.2f}%')
-        print(f'Average loss of the model on test data: {average_loss:.4f}')
+        average_loss = total_loss / len(test_loader)
+        
+        result = f"Loss: {average_loss:.4f}"
+        
+        for metric_name, metric in zip(metrics.keys(), metrics.values()):
+            result += f', {metric_name}: {metric.compute().item():.2f}'
+        print(result)
--- utils/train.py
+++ utils/train.py
@@ -3,12 +3,15 @@ import json
 import matplotlib.pyplot as plt
 
 def visualize_logs():
-    with open('history/training_logs.json', 'r') as f:
-        logs = json.load(f)
+    with open('history/train_logs.json', 'r') as f:
+        train_logs = json.load(f)
+    
+    with open('history/validation_logs.json', 'r') as f:
+        validate_logs = json.load(f)
 
     plt.figure(figsize=(10, 5))
-    plt.plot(logs['train_loss'], label='Train Loss')
-    plt.plot(logs['val_loss'], label='Validation Loss')
+    plt.plot(train_logs['Loss'], label='Train Loss')
+    plt.plot(validate_logs['Loss'], label='Validation Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title('Train and Validation Loss')
@@ -17,8 +20,8 @@ def visualize_logs():
     plt.close()
 
     plt.figure(figsize=(10, 5))
-    plt.plot(logs['train_accuracy'], label='Train Accuracy')
-    plt.plot(logs['val_accuracy'], label='Validation Accuracy')
+    plt.plot(train_logs['Accuracy'], label='Train Accuracy')
+    plt.plot(validate_logs['Accuracy'], label='Validation Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.title('Train and Validation Accuracy')
@@ -26,77 +29,89 @@ def visualize_logs():
     plt.savefig('history/accuracy.png')
     plt.close()
     
-def train(num_epochs, train_loader, valid_loader, classifier, criterion, optimizer, device):
+def train(num_epochs, train_loader, valid_loader, classifier, metrics, criterion, optimizer, device):
     best_val_loss = float('inf')
-
-    history_logs = {
-        'train_loss': [],
-        'train_accuracy': [],
-        'val_loss': [],
-        'val_accuracy': []
-    }
-
+    history_train_logs = {'Loss': []}
+    history_val_logs = {'Loss': []}
+    
     for epoch in range(num_epochs):
+        #training
         classifier.train()
-        correct_train = 0
-        total_train = 0
-        total_train_loss = 0.0  
+        total_train_loss = 0.0
+        for metric in metrics.values():
+            metric.reset()
+        
         for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
             
             outputs = classifier(images)
             loss = criterion(outputs, labels)
-            
             _, predicted = torch.max(outputs.data, 1)
-
-            total_train += labels.size(0)
-            correct_train += (predicted == labels).sum().item()
-            total_train_loss += loss.item()  
-
-            optimizer.zero_grad()  
+            
+            total_train_loss += loss.item()
+            for metric in metrics.values():
+                metric.update(predicted, labels)
+            
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
         average_train_loss = total_train_loss / len(train_loader)
-        train_accuracy = (correct_train / total_train) * 100
-
-        classifier.eval()
+        history_train_logs['Loss'].append(average_train_loss)
+                
+        result = f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {average_train_loss:.4f}"
+        
+        for metric_name, metric in zip(metrics.keys(), metrics.values()):
+            metric_value = metric.compute().item()
+            result += f', {metric_name}: {metric_value:.2f}'
+            if epoch == 0:
+                history_train_logs[f'{metric_name}'] = [metric_value]
+            else:
+                history_train_logs[f'{metric_name}'].append(metric_value)
+            
+        #validating
         with torch.no_grad():
-            correct = 0
-            total = 0
+            classifier.eval()
             total_val_loss = 0.0
-
+            for metric in metrics.values():
+                metric.reset()
+            
             for images, labels in valid_loader:
                 images, labels = images.to(device), labels.to(device)
                 
                 outputs = classifier(images)
                 val_loss = criterion(outputs, labels)
-
                 _, predicted = torch.max(outputs.data, 1)
 
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
                 total_val_loss += val_loss.item()
+                for metric in metrics.values():
+                    metric.update(predicted, labels)
 
                 del images, labels, outputs
 
-            accuracy = (correct / total) * 100
             average_val_loss = total_val_loss / len(valid_loader)
-
-        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {average_train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}%, Val Loss: {average_val_loss:.4f}, Val Accuracy: {accuracy:.2f}%")
-
-        history_logs['train_loss'].append(average_train_loss)
-        history_logs['train_accuracy'].append(train_accuracy)
-        history_logs['val_loss'].append(average_val_loss)
-        history_logs['val_accuracy'].append(accuracy)
-
+            history_val_logs['Loss'].append(average_train_loss)
+            
+            result += f'Val Loss: {average_val_loss:.4f}'
+            for metric_name, metric in zip(metrics.keys(), metrics.values()):
+                metric_value = metric.compute().item()
+                result += f', Val {metric_name}: {metric_value:.2f}'
+                if epoch == 0:
+                    history_val_logs[f'{metric_name}'] = [metric_value]
+                else:
+                    history_val_logs[f'{metric_name}'].append(metric_value)
+        
+        print(result)
         if average_val_loss < best_val_loss:
             torch.save(classifier.state_dict(), 'weights/best.pt')
             best_val_loss = average_val_loss
 
     torch.save(classifier.state_dict(), 'weights/last.pt')
 
-    with open('history/training_logs.json', 'w') as f:
-        json.dump(history_logs, f, indent=4)
+    with open('history/train_logs.json', 'w') as f:
+        json.dump(history_train_logs, f, indent=4)
+        
+    with open('history/validation_logs.json', 'w') as f:
+        json.dump(history_val_logs, f, indent=4)
 
     visualize_logs()
